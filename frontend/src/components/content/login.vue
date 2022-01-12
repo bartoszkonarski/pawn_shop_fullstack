a1c88361ddcf8bd98bb9a7ce8af4f6f6ade0f2c4
parogracz <dnasser113@gmail.com>
2022-01-12 01:39:54 +0100

    work on conn with serv

--- frontend/src/components/content/login.vue
+++ frontend/src/components/content/login.vue
@@ -1,27 +1,42 @@
 <template>
   <section>
-      <form action="http://127.0.0.1:5000/login" method="POST">
-        <input type="text" name="username">
-        <input type="text" name="password">
+      <form @submit="postMethod">
+        <input type="text" name="username" v-model="login">
+        <input type="text" name="password" v-model="password">
         <input type="submit"/>
       </form>
   </section>
 </template>
 
 <script>
-export default {
-  name: 'Login',
-  components: {
-  },
-  props: ['logged'],
-  data: function() {
-    return{
-    }
-  },
-  beforeMount(){
-    if(this.logged == true) window.location.href = "http://localhost:8080/#/";
-  },
-}
+  import axios from 'axios';
+  export default {
+    
+    name: 'Login',
+    components: {
+    },
+    props: ['logged'],
+    data: function() {
+      return{
+        login:"",
+        password:"",
+      }
+    },
+    methods: {
+      postMethod(){
+        axios.post(
+        'http://127.0.0.1:5000/login',
+        { username: this.login, password: this.password},
+        { headers: {'Content-Type': 'application/json'}}
+        );
+        alert("bump");
+      }
+      
+    },
+    beforeMount(){
+      if(this.logged == true) window.location.href = "http://localhost:8080/#/";
+    },
+  }
 </script>
 
 <style>
